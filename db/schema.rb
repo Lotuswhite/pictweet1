29f471138b029abe37d171f519a80fe70ce6cef4
Lotuswhite <renisawa0105@icloud.com>
2020-10-22 15:06:44 +0900

    ビューの調整、変更

--- db/schema.rb
+++ db/schema.rb
@@ -33,14 +33,6 @@ ActiveRecord::Schema.define(version: 2020_10_15_110516) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
-    t.integer "info_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_items_on_user_id"
-  end
-
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.string "text"
     t.text "image"
@@ -52,6 +44,9 @@ ActiveRecord::Schema.define(version: 2020_10_15_110516) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "first_name", default: "", null: false
+    t.string "family_name", default: "", null: false
+    t.string "sex", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -62,5 +57,4 @@ ActiveRecord::Schema.define(version: 2020_10_15_110516) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "items", "users"
 end
